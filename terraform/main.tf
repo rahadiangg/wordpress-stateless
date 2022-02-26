5912c6b75b871aa22f21a8c9afaa3d76f7d8d598
rahadiangg <dianraha11@gmail.com>
2022-02-26 21:47:46 +0700

    setup load balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -315,3 +315,87 @@ resource "google_compute_region_autoscaler" "autoscaler" {
     }
   }
 }
+
+
+
+// Static IP address untuk load balancer
+resource "google_compute_global_address" "address" {
+  name = "static-ip-${var.project_name}"
+}
+
+
+// setup load balancer
+module "lb-http" {
+  source  = "GoogleCloudPlatform/lb-http/google"
+  version = "6.2.0"
+  
+  project = var.project_id
+  name              = "${var.project_name}-lb"
+  target_tags       = ["allow-hc"]
+
+  ssl               = true
+  managed_ssl_certificate_domains  = ["wp.rahadian.dev"]
+  use_ssl_certificates = false
+  create_address = false
+  address = google_compute_global_address.address.self_link
+
+  # http_forward = false
+  
+  backends = {
+    default = {
+      description                     = null
+      protocol                        = "HTTP"
+      port                            = 80
+      port_name                       = "http"
+      timeout_sec                     = 10
+      connection_draining_timeout_sec = null
+      enable_cdn                      = false
+      security_policy                 = null
+      session_affinity                = null
+      affinity_cookie_ttl_sec         = null
+      custom_request_headers          = null
+      custom_response_headers         = null
+
+      enable_cdn                      = true
+
+      health_check = {
+        check_interval_sec  = 10
+        timeout_sec         = 5
+        healthy_threshold   = 2
+        unhealthy_threshold = 3
+        request_path        = "/"
+        port                = 80
+        host                = null
+        logging             = null
+      }
+
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
+
+      groups = [
+        { 
+          group                        = google_compute_region_instance_group_manager.app.instance_group
+          balancing_mode               = null
+          capacity_scaler              = null
+          description                  = null
+          max_connections              = null
+          max_connections_per_instance = null
+          max_connections_per_endpoint = null
+          max_rate                     = null
+          max_rate_per_instance        = null
+          max_rate_per_endpoint        = null
+          max_utilization              = null
+        }
+      ]
+
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = null
+        oauth2_client_secret = null
+      }
+    }
+  }
+
+}
